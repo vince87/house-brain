cdfd84b2a1b04e4a05983ad0983494bae5b7ba2f
vince87 <vincenzo.bini@gmail.com>
2026-08-23 18:33:56 +0200

    style: organize panel bridge imports

--- custom_components/house_brain/websocket.py
+++ custom_components/house_brain/websocket.py
@@ -5,11 +5,10 @@ from __future__ import annotations
 from typing import Any
 from urllib.parse import quote
 
-import voluptuous as vol
-
 from homeassistant.components import websocket_api
 from homeassistant.components.websocket_api import ActiveConnection
 from homeassistant.core import HomeAssistant, callback
+import voluptuous as vol
 
 from .api import (
     HouseBrainApiError,
